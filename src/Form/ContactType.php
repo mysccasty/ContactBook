d8d8c3a2aed29d118961468187f07967c770c5d6
mysccasty <morcasersa@gmail.com>
2024-02-25 16:01:44 +0300

    Add crud contact controller

---
+++ src/Form/ContactType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('phoneNumber')
+            ->add('email')
+            ->add('photoFilename')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('username', EntityType::class, [
+                'class' => User::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
